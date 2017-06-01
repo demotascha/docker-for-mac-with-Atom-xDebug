b072b67d90aec72ba85735c67d4fa0cec828e768
Huang Wei-Hao <demotascha@gmail.com>
2017-06-01 13:59:08 +0800

    add composer.sh

---
+++ composer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /tmp
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php -r "unlink('composer-setup.php');"
+
+composer global require "hirak/prestissimo:^0.3"
